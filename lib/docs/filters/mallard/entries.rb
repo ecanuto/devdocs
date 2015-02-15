f4fdedfc1fb0922abe1c19b058f17168fb8afb2f
Philip Chimento <philip.chimento@gmail.com>
2015-02-15 00:14:26 -0800

    Organize pages in sidebar
    For now, we organize methods, properties, signals, etc. under the heading
    of their class name. We use some inferences for things that don't belong
    to classes:
     - if it starts with a lowercase letter, it's a function.
     - if it has "Func" in it, it's a function typedef. (Misses GCallback!)
     - if it's all uppercase, it's a constant.
    These we file under special headings (in parentheses so they show up
    first alphabetically.)

--- lib/docs/filters/mallard/entries.rb
+++ lib/docs/filters/mallard/entries.rb
@@ -3,6 +3,10 @@ module Docs
     # EntriesFilter for Mallard documentation. This figures out the metadata for
     # each page.
     class EntriesFilter < Docs::EntriesFilter
+      FUNCTIONS_HEADING = '(Functions)'
+      FUNCTION_TYPES_HEADING = '(Function Types)'
+      CONSTANTS_HEADING = '(Constants)'
+
       # "name" is the title that the documentation page has in the left sidebar.
       def get_name
         node = at_css('h1.title')
@@ -13,7 +17,12 @@ module Docs
       # "type" is the heading that the documentation page is displayed under, in
       # the left sidebar.
       def get_type
-        name
+        namespace, object, method = *slug.split('.')
+        object, property = *object.split('-', 2)
+        return FUNCTIONS_HEADING if object.match(/^[a-z]/)
+        return FUNCTION_TYPES_HEADING if object.match(/Func$/)
+        return CONSTANTS_HEADING unless object.match(/[a-z]/)
+        object
       end
     end
   end
