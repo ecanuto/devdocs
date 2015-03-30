c27c7d0a624a45a85a945267ef3ef90445e90315
Philip Chimento <philip.chimento@gmail.com>
2015-03-29 21:34:21 -0700

    Automatically create scrapers from GIR files
    This adds a `thor gir:generate` command which reads a GIR file and
    generates a corresponding scraper in the directory lib/docs/scrapers/
    gnome/generated/. Instead of relying on built Mallard HTML documentation
    in a particular directory, it builds the Mallard HTML documentation with
    g-ir-doc-tool and yelp-build during the initialization process of the
    scraper, in a temporary directory.

--- lib/docs/scrapers/gnome/mallard.rb
+++ lib/docs/scrapers/gnome/mallard.rb
@@ -3,6 +3,12 @@ module Docs
     require 'docs/filters/mallard/fix_gtkdoc'
     attr_accessor :preprocessor
 
+    class << self
+      attr_accessor :c_prefix
+      attr_accessor :api_version
+      attr_accessor :gir_path
+    end
+
     self.type = 'mallard'
     self.abstract = true
     self.root_path = 'index.html'
@@ -14,6 +20,35 @@ module Docs
     def initialize
       super
       @preprocessor = FixGtkdocFilter.new
+      self.class.dir = run_doctool self.class.gir_path
+    end
+
+    def doctool
+      ENV['G_IR_DOC_TOOL'].nil? ? 'g-ir-doc-tool' : ENV['G_IR_DOC_TOOL']
+    end
+
+    def yelp
+      ENV['YELP_BUILD'].nil? ? 'yelp-build' : ENV['YELP_BUILD']
+    end
+
+    def run_doctool(gir_path)
+      puts 'Generating Mallard documentation...'
+      Dir.mktmpdir do |dir|
+        unless system "#{doctool} #{gir_path} -o #{dir} -l gjs"
+          fail 'g-ir-doc-tool failed'
+        end
+        run_yelp dir
+      end
+    end
+
+    def run_yelp(mallard_path)
+      puts 'Generating HTML documentation...'
+      dir = Dir.mktmpdir
+      unless system "#{yelp} html -o #{dir} #{mallard_path}"
+        FileUtils.remove_entry dir
+        fail 'yelp-build failed'
+      end
+      dir
     end
 
     def read_file(path)
