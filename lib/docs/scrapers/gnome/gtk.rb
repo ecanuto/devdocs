cba9b5ed0b836e1da761f125da4208f70f18b75d
Philip Chimento <philip.chimento@gmail.com>
2015-03-29 21:34:21 -0700

    Automatically create scrapers from GIR files
    This adds a `thor gir:generate` command which reads a GIR file and
    generates a corresponding scraper in the directory lib/docs/scrapers/
    gnome/generated/. Instead of relying on built Mallard HTML documentation
    in a particular directory, it builds the Mallard HTML documentation with
    g-ir-doc-tool and yelp-build during the initialization process of the
    scraper, in a temporary directory.

--- lib/docs/scrapers/gnome/gtk.rb
+++
@@ -1,9 +0,0 @@
-module Docs
-  class Gtk < Mallard
-    self.name = 'GTK+'
-    self.slug = 'gtk'
-    self.version = '3.14'
-    # FIXME: hardcoded
-    self.dir = '/Users/fliep/gtk/source/gobject-introspection/docs-built/'
-  end
-end
